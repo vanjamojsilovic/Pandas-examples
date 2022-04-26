1a0e05a060c711fd98246cd48167ab28e1df7ef4
Vanja Mojsilovic <vanja.mojsilovic@gmail.com>
2022-04-26 23:33:13 +0200

    2332

--- read_modify_and_save.py
+++ read_modify_and_save.py
@@ -4,4 +4,7 @@ from openpyxl import load_workbook
 workbook = load_workbook("created_excel_file.xlsx")
 sheet = workbook.active
 sheet["A1"] = "Changed_text"
+workbook.create_sheet("New_sheet")
+sheet = workbook.get_sheet_by_name('New_sheet')
+sheet["B1"] = "text in new sheet"
 workbook.save("created_excel_file_result.xlsx")
